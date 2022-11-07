6184a2192f7cf7ddbe544439b18e5e9d8871a798
nownabe <nownabe@users.noreply.github.com>
2022-11-07 15:46:16 +0900

    Put logs to stdout

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import logging
 import os
+import sys
+
 from dinosaur.app import create_app
+
+
+logger = logging.getLogger('uvicorn')
+
+for handler in logger.handlers:
+    handler.setStream(sys.stdout)
+
 
 firebase_config_json = os.environ["FIREBASE_CONFIG"]
 
